13a01bfc3308acba51ca69bb0e08ad5bd853210b
Angel Zamorano <angel.zamorano@innova-ti.cl>
2023-05-23 14:02:10 -0400

    se termina ejercicio 1

--- ejercicio-1.py
+++ ejercicio-1.py
@@ -1,19 +1,35 @@
-def validadorClave(clave:str):
-   
-    
-    
-    if bool(clave.isalpha()):
-        return True
-    else:
-        return False
-    
-    
+def validaMayuscula(clave:str):
+    mayuscula=False
+    for valida in clave:
+        if valida.isupper():
+            mayuscula=True
 
+    return mayuscula
+
+def validaMinuscula(clave:str):
+    minuscula=False
+    for valida in clave:
+        if valida.islower():
+            minuscula=True
+    return minuscula
+    
+def validaDigito(clave:str):
+    digito=False
+    for valida in clave:
+        if valida.isdigit():
+            digito=True
+    return digito
+    
+if __name__=='__main__':
+    ingresoContrasena = input("Ingrese una palabra: ")
+    if ingresoContrasena.isalnum():
+        if validaMayuscula(ingresoContrasena) and validaMinuscula(ingresoContrasena) and validaDigito(ingresoContrasena):
+            response = "Contrasena Segura"
+        else: 
+            response = "Contrasena insegura"
+            
         
+    else: 
+        response= "La contrasena a verificar no debe tener caracter extranos"   
     
-    
-    
-
-ingreso = input("Ingrese una palabra: ")
-response = validadorClave(ingreso)
-print(response)
+    print(response)
