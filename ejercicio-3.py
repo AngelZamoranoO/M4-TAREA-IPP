4f53450c34d0dfc9094808f0fc15b5d7e01c5d92
Angel Zamorano <angel.zamorano@innova-ti.cl>
2023-05-24 03:13:45 -0400

    Se termino el ejercicio 3

--- ejercicio-3.py
+++ ejercicio-3.py
@@ -1,3 +1,5 @@
+from os import system
+import time
 class Cuenta():
     #atributos de la clase
     number_account:int
@@ -39,10 +41,44 @@ if __name__=='__main__':
         print('*************** CUENTA BANCARIA ***************')
         cuenta =  Cuenta()
         
-        cuenta.
+        cuenta.number_account=int(input('Ingrese numero de cuenta: \n'))
+        cuenta.headline_name=str(input('Ingrese nombre del titular: \n'))
+        cuenta.initial_balance=int(input('Ingrese el monto inicial: \n'))
+        cuenta.account_type=str(input('Ingrese el tipo de cuenta: \n'))
+        
+        system('cls')
+        
         while True:
             
-        
+            print('Opcion 1 => DEPOSITAR')
+            print('Opcion 2 => RETIRAR')
+            print('Opcion 3 => BALANCE')
+            print('Opcion 4 => Salir')
+            opcion=int(input('Numero Opcion => '))
+            
+            if opcion==1:
+                deposito=int(input('1) Ingrese el monto a depositar => $'))
+                print(cuenta.deposit(deposito))
+                time.sleep(5)
+                system('cls')
+            elif opcion==2:
+                retiro=int(input('1) Ingrese el monto a retiro => $'))
+                print(cuenta.withdraw(retiro))
+                time.sleep(5)
+                system('cls')
+            elif opcion==3:
+                print(cuenta.balance())
+                time.sleep(5)
+                system('cls')
+            elif opcion==4:
+                break
+                
+            else:
+                print('La opcion ingresar no existe, vuelve a intentarlo')
+                time.sleep(5)
+                system('cls')
+            
+        print('FIN DEL PROGRAMA')         
     except ValueError as e:
         print('El valor ingresaro no exite, ValueError =>',e)
         
